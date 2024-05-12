abbdc221395b7618635be2e4876d1a55762719d4
2091527886 <2091527886@qq.com>
2024-05-12 14:30:44 +0800

    Update brutal.c

--- brutal.c
+++ brutal.c
@@ -11,10 +11,10 @@
  and your kernel version is greater than 5.8.
 #endif
 
-#define INIT_PACING_RATE 125000 // 1 Mbps
-#define INIT_CWND_GAIN 20
+#define INIT_PACING_RATE 1250000 // 1 Mbps
+#define INIT_CWND_GAIN 30
 
-#define MIN_PACING_RATE 62500 // 500 Kbps
+#define MIN_PACING_RATE 625000 // 500 Kbps
 #define MIN_CWND_GAIN 5
 #define MAX_CWND_GAIN 80
 #define MIN_CWND 4
